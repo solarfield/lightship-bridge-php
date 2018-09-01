46fbb9486fb658be7fc4cd0ea096fcae50988461
mikecaines <mikecaines@gmail.com>
2018-09-01 12:19:00 -0300

    delete global App.stub after using

--- docs/example-files/index.js
+++ docs/example-files/index.js
@@ -1,4 +1,8 @@
 System.import('solarfield/lightship-js/src/Solarfield/Lightship/Bootstrapper')
-.then(function (Bootstrapper) {
-	return Bootstrapper.go(App.stub);
-});
+	.then(function (Bootstrapper) {
+		return (System.isModule(Bootstrapper) ? Bootstrapper.default : Bootstrapper)
+			.go(App.stub)
+			.then(function () {
+				delete App.stub;
+			});
+	});
