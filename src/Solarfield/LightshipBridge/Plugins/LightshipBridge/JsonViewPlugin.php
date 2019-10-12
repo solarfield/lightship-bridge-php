91466c8fc8beb1dbdb08042316a2834108a8053a
Mike <mikecaines@gmail.com>
2019-10-11 21:07:12 -0300

    reworking of lightship-bridge handling of stdout messages
    This is a stopgap solution until some other lightship stuff is
    completed, such as Environment plugins.

---
+++ src/Solarfield/LightshipBridge/Plugins/LightshipBridge/JsonViewPlugin.php
@@ -0,0 +1,21 @@
+<?php
+namespace Solarfield\LightshipBridge\Plugins\LightshipBridge;
+
+use Solarfield\Lightship\Events\CreateJsonDataEvent;
+use Solarfield\Lightship\ViewInterface;
+
+class JsonViewPlugin extends \Solarfield\Lightship\JsonViewPlugin {
+	public function __construct(ViewInterface $aView, $aComponentCode) {
+		parent::__construct($aView, $aComponentCode);
+
+		$this->getView()->addEventListener('create-json-data', function (CreateJsonDataEvent $aEvt) {
+			if (($stdoutMessages = $this->getView()->getEnvironment()->takeBufferedStdoutMessages())) {
+				$aEvt->getJsonData()->mergeInto([
+					'app' => [
+						'stdoutMessages' => $stdoutMessages,
+					],
+				]);
+			}
+		});
+	}
+}
